cbf6daf2d99ccbae3b79507935054d92863d2961
Jeremy Myers <inernestmusic@gmail.com>
2021-07-24 15:01:02 -0500

    built out array methods to filter and store validMoves that are to the right, left, top, and bottom of an initialHit for the AI computer fxnality. AI will use these arrays to determine the direction of a hit ship and to sink it more intelligently than the current randomMove fxnality of the computer.

--- src/gameHandler.js
+++ src/gameHandler.js
@@ -76,12 +76,126 @@ function createValidMovesArray() {
   return validMoves;
 }
 
-function generateComputerAttack() {
-  const randomIndex = Math.floor(Math.random() * getValidMoves.length);
-  const randomMove = getValidMoves[randomIndex];
-  getValidMoves.splice(randomIndex, 1);
-  return randomMove;
+// function generateComputerAttack() {
+//   const randomIndex = Math.floor(Math.random() * getValidMoves.length);
+//   const randomMove = getValidMoves[randomIndex];
+//   getValidMoves.splice(randomIndex, 1);
+//   return randomMove;
+// }
+let isAITriggered = false;
+let initialCPUHitArray = [];
+
+function getValidAdjacentCPUMoves(initialHit) {
+  console.log(initialHit);
+  getMovesRight(initialHit);
+  getMovesLeft(initialHit);
+  getMovesDown(initialHit);
+  getMovesUp(initialHit);
 }
+
+function getMovesRight(hit) {
+  const getIndex = getValidMoves.indexOf(hit);
+  const movesRight = getValidMoves.slice(getIndex).filter((item, index) => {
+    if (item - hit - index === 0 && item < (Math.floor(hit / 10) + 1) * 10) {
+      return item;
+    }
+  });
+  movesRight.shift();
+  console.log({ movesRight });
+}
+
+function getMovesLeft(hit) {
+  const getIndex = getValidMoves.indexOf(hit);
+  const mapValidMoves = getValidMoves.map((item) => {
+    return item;
+  });
+  const movesLeft = [];
+  mapValidMoves.splice(getIndex, mapValidMoves.length - 1);
+  mapValidMoves.reverse().filter((item, index) => {
+    if (hit === item + index + 1 && item >= Math.floor(hit / 10) * 10) {
+      movesLeft.push(item);
+    }
+  });
+  console.log({ movesLeft });
+}
+
+function getMovesDown(hit) {
+  const verticalMoves = getValidMoves.filter((coord) => {
+    if (
+      hit - Math.floor(hit / 10) * 10 ===
+      coord - Math.floor(coord / 10) * 10
+    ) {
+      return coord;
+    }
+  });
+  const movesDown = verticalMoves
+    .slice(verticalMoves.indexOf(hit))
+    .filter((coord, index) => {
+      if (coord - hit - index * 10 === 0) {
+        return coord;
+      }
+    });
+  movesDown.shift();
+  console.log({ movesDown });
+}
+
+function getMovesUp(hit) {
+  const verticalMoves = getValidMoves.filter((coord) => {
+    if (hit % 10 === coord % 10) {
+      return coord;
+    }
+  });
+  verticalMoves.reverse();
+
+  const movesUp = verticalMoves
+    .slice(verticalMoves.indexOf(hit))
+    .filter((coord, index) => {
+      if (hit === coord + index * 10) {
+        return coord;
+      }
+    });
+  if (
+    hit % 10 === 0 &&
+    getValidMoves.includes(0) &&
+    hit === movesUp.length * 10
+  ) {
+    movesUp.push(0);
+  }
+  movesUp.shift();
+  console.log({ movesUp });
+}
+
+function generateComputerAttack() {
+  let cpuMove;
+  if (!isAITriggered) {
+    const randomIndex = Math.floor(Math.random() * getValidMoves.length);
+    cpuMove = getValidMoves[randomIndex];
+    // getValidMoves.splice(randomIndex, 1);
+  } else {
+    console.log(`begin smart move`);
+    // console.log(initialCPUHitArray);
+    // cpuMove = getSmartCPUMove();
+    // getValidMoves.splice(getValidMoves.indexOf(cpuMove), 1);
+  }
+  return cpuMove;
+}
+
+// let isVert;
+// let areShipsAdjacent;
+// function getSmartCPUMove() {
+//   if (
+//     getValidMoves.includes(initialCPUHitArray[1] + 1) &&
+//     initialCPUHitArray[1] + 1 <
+//       (Math.floor(initialCPUHitArray[1] / 10) + 1) * 10
+//   ) {
+//     return initialCPUHitArray[1] + 1;
+//   } else if (
+//     getValidMoves.includes(initialCPUHitArray[1] - 1) &&
+//     initialCPUHitArray[1] - 1 > Math.floor(initialCPUHitArray[1] / 10) * 10
+//   ) {
+//     return initialCPUHitArray[1] - 1;
+//   }
+// }
 // END ----- generates random move for computer ----------- //
 
 function gameLoop(playerMove) {
@@ -101,23 +215,38 @@ function gameLoop(playerMove) {
         coordOfAttack = generateComputerAttack();
       }
       const attackOutcome = receiveAttack(coordOfAttack, getTurn);
-      console.log(attackOutcome);
+      // console.log(attackOutcome);
       renderMove(getTurn, attackOutcome);
       if (attackOutcome[0]) {
-        console.log(storedGameboards);
+        // console.log(attackOutcome);
+        // console.log(storedGameboards);
         storedGameboards.filter((item) => {
           if (item[0] === getTurn) {
             isGameOver = item[1].isGameOver();
           }
         });
       }
+      if (getTurn === `computer`) {
+        if (attackOutcome[0] && getTurn === `computer`) {
+          isAITriggered = true;
+          storedGameboards[1][1].ships.fleet.filter((object) => {
+            if (attackOutcome[0] === object.name && object.hits.length === 1)
+              console.log(`first hit`);
+            initialCPUHitArray = [object, attackOutcome[1]];
+          });
+          const validSmartMoves = getValidAdjacentCPUMoves(attackOutcome[1]);
+          // console.log(validSmartMoves);
+        }
+        getValidMoves.splice(getValidMoves.indexOf(attackOutcome[1]), 1);
+      }
+
       if (attackOutcome[0] && getTurn === `player`) {
         let isShipSunk;
-        let arrayIndex;
+        // let arrayIndex;
         storedGameboards[0][1].ships.fleet.filter((object, index) => {
           if (attackOutcome[0] === object.name) {
             isShipSunk = object.isSunk;
-            arrayIndex = index;
+            // arrayIndex = index;
             if (isShipSunk) {
               const cpuHiddenShips =
                 document.querySelectorAll(`.cpu-ships-rendered`);
